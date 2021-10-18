aff9aa84a885b93563c2c535eae962a621a190a3
Ville Ritala <ritalaville@gmail.com>
2021-10-18 13:51:44 +0300

    changes in tests

--- src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
+++ src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
@@ -7,23 +7,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import hh.swd20.bookstore.Domain.BookRepository;
 import hh.swd20.bookstore.controller.BookController;
 import hh.swd20.bookstore.controller.CategoryController;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class BookstoreApplicationTests {
+public class BookstoreApplicationTests {
 	
 	@Autowired
 	private BookController bookController;
+	
+	@Autowired
 	private CategoryController categoryController;
 
 
 	@Test
 	public void contextLoads() throws Exception {
 		assertThat(bookController).isNotNull();
+	}
+	@Test
+	public void contextLoads1() throws Exception {
 		assertThat(categoryController).isNotNull();
 	}
-
 }
